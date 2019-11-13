e75ee1568982fcdb02e2897219f0bf88bafaa87d
DiasJPaullo <joaopaullodias@gmail.com>
2019-11-13 09:41:31 -0200

    inicio do projeto 13.11.2019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {LancamentosComponent} from './lancamentos/lancamentos.component';
+import { from } from 'rxjs';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: AppComponent
+  },
+  {
+    path: 'home',
+    component: AppComponent
+  },
+
+  {
+    path: 'index',
+    component: AppComponent
+  },
+  {
+    path: 'lancamentos',
+    component: LancamentosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
