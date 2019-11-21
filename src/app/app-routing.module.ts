9bcaae49a490940d48d58766a4ade2d236d06dc8
DiasJPaullo <joaopaullodias@gmail.com>
2019-11-21 12:40:46 -0200

    add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,18 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import {LancamentosComponent} from './lancamentos/lancamentos.component';
+import{HomeComponent} from './home/home.component';
 import { from } from 'rxjs';
 
 
 const routes: Routes = [
-  {
-    path:'',
-    component: AppComponent
-  },
-  {
-    path: 'home',
-    component: AppComponent
-  },
-
-  {
-    path: 'index',
-    component: AppComponent
-  },
-  {
+ {
     path: 'lancamentos',
     component: LancamentosComponent
+  },
+ {
+    path: 'lancamentos',
+    component: HomeComponent
   }
 ];
 
